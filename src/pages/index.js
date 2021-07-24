e6122f3e0a0932782f794d09981a7702a7628a62
Lyric <5h3ll3x@gmail.com>
2021-07-24 17:01:58 +0900

    adjust color

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,18 @@ import HomepageFeatures from '../components/HomepageFeatures';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg padding-horiz--md margin-horiz--md"
+            className="button button--primary button--lg padding-horiz--md margin-horiz--sm"
             to="/docs/intro">
             User Manuals
           </Link>
           <Link
-            className="button button--primary-light button--lg padding-horiz--md margin-horiz--md"
+            className="button button--secondary button--lg padding-horiz--md margin-horiz--sm"
             to="/docs/developer/intro">
             Developer Docs
           </Link>
