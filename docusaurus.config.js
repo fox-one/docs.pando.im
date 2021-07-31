16860bf5c0a64ce910719d3763520fb7ff0a2f42
Lyric <5h3ll3x@gmail.com>
2021-07-31 11:08:49 +0900

    fix: doc search options

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,10 +24,10 @@ module.exports = {
       contextualSearch: true,
 
       // Optional: see doc section below
-      appId: 'YOUR_APP_ID',
+      // appId: 'YOUR_APP_ID',
 
       // Optional: Algolia search parameters
-      searchParameters: {},
+      // searchParameters: {},
 
       //... other Algolia params
     },
